546e87d67902ed4bbbf3c660e374d96ce168b850
soyudea <omara.torres@udea.edu.co>
2024-03-08 20:19:41 -0500

    Se carga funciones opcion

--- reverse.c
+++ reverse.c
@@ -44,3 +44,47 @@ int main(int argc, char *argv[])
     flujoPrograma(argc, argv);
     return 0;
 }
+
+/**
+ *     La funcion corresponde a la opcion "./reverse"
+*/
+
+void opcionUno() {
+    textInputUser();
+}
+
+/**
+ * La funcion corresponde a la opcion "./reverse input.txt output.txt"
+*/
+void opcionDos(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Uso: %s <archivo>\n", argv[0]);
+        exit(1);
+    }
+
+    FILE *archivo = fopen(argv[1], "r");
+    if (archivo == NULL) {
+        fprintf(stderr, "error: cannot open file '%s'\n", argv[1]);
+        exit(1);
+    }
+
+    int numLineas;
+    char **lineas = leerArchivo(archivo, &numLineas);
+    fclose(archivo);
+    invertirLineas(lineas, numLineas);      //Invierte lineas de texto
+    imprimirLineas(lineas, numLineas);      //Imprime salida estandar
+    liberarMemoria(lineas, numLineas);      //libera memoria 
+}
+
+/**
+ * La funcion corresponde a la opcion "./reverse namefile.txt nemefile.txt"
+*/
+void opcionTres(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "usage: %s reverse <input> <output>", argv[0]);
+        exit(1);
+    }
+
+    procesoArchivos(argv[1], argv[2]);
+}
+
